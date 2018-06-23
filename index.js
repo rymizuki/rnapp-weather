052fbb9ae2c02afbf9eba485f44842f974c3ad4a
mizuki_r <ry.mizuki@gmail.com>
2018-06-23 15:10:35 +0900

    chore(build): fixed uninstalled module.

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
-AppRegistry.registerComponent('rnappweather', () => App);
+AppRegistry.registerComponent('Weather', () => App);
