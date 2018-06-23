3243948d319150565bc3dc2083e16d77cc94e8d6
mizuki_r <ry.mizuki@gmail.com>
2018-06-23 15:44:12 +0900

    chore(ts): add TypeScript

---
+++ rn-cli.config.js
@@ -0,0 +1,11 @@
+const extraNodeModules = require('node-libs-react-native')
+
+module.exports = {
+  extraNodeModules,
+  getTransformModulePath() {
+    return require.resolve('react-native-typescript-transformer');
+  },
+  getSourceExts() {
+    return ['ts', 'tsx'];
+  }
+}
